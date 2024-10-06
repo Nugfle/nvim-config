e6f5a5cd6e24516e97571f2d6f46c0afac65ebda
Niggi1105 <niklas.liesch17@gmail.com>
2024-10-06 21:21:31 +0200

    setup 	new file:   init.lua 	new file:   lazy-lock.json 	new file:   lua/core/init.lua 	new file:   lua/core/lazy.lua 	new file:   lua/core/remap.lua 	new file:   lua/core/settings.lua 	new file:   lua/plugins/autopairs.lua 	new file:   lua/plugins/colorscheme.lua 	new file:   lua/plugins/futgitive.lua 	new file:   lua/plugins/harpoon.lua 	new file:   lua/plugins/lsp.lua 	new file:   lua/plugins/telescope.lua 	new file:   lua/plugins/treesitter.lua 	new file:   lua/plugins/undotree.lua

---
+++ lua/core/remap.lua
@@ -0,0 +1,16 @@
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader><leader>', vim.cmd.so)
+
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '>-1<CR>gv=gv")
+
+vim.keymap.set('n', '<leader>J', "mzJ`z")
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+vim.keymap.set('x', '<leader>pp', "\"_dP")
+
+vim.keymap.set('n', '<leader>d', "\"_d")
+vim.keymap.set('v', '<leader>d', "\"_d")
